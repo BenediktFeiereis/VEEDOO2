473c5691155c07764a17ee3cdcf47f83a39a6cab
BenediktFeiereis <feiereisbenedikt@gmail.com>
2025-04-18 14:25:40 +0200

    VEEDOO 2
    erste Version der VEEDOO 2 nach Thieme

---
+++ script.js
@@ -0,0 +1,8 @@
+function startApp() {
+    document.getElementById('Protkoll');
+}
+function startApp() {
+    document.getElementById("startScreen").classList.add("hidden");
+    document.getElementById("nextScreen").classList.remove("hidden");
+    document.getElementById("Einsatzmanagement").classList.remove("hidden");
+  }
